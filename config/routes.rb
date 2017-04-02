d41bb9d73892a8fa34458d3bb9f1aa0d55a93cd4
Yan Yin Choy <yanyinchoy@gmail.com>
2017-04-02 08:46:31 -0700

    est the socket connection on server-side

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'users#index'
-
+  # Serve websocket cable requests in-process
+  mount ActionCable.server => '/cable'
+  
   ## SESSIONS ##
   get "/login", to: "sessions#new"
 
@@ -10,6 +12,6 @@ Rails.application.routes.draw do
 
   resources :users, except: [:index]
 
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
