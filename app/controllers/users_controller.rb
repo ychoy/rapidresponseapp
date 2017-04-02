bf775061cd4b54b8b83e22f7e279b8ad69f5920f
Yan Yin Choy <yanyinchoy@gmail.com>
2017-04-01 22:17:50 -0700

    adds routes for users, changes password field

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,16 +25,9 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        redirect_to root_path
       end
-    end
   end
 
   # PATCH/PUT /users/1
