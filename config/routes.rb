77cfa6681919c731a4459f1a4d5353f54f2cd064
Yan Yin Choy <yanyinchoy@gmail.com>
2017-04-02 11:57:07 -0700

    routes for memberships

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,13 @@ Rails.application.routes.draw do
 
   resources :users, except: [:index]
 
-  resources :conversations, except: [:edit, :update, :destroy]
+  resources :conversations
 
   resources :messages, except: [:edit, :update, :destroy]
+
+  get '/users/:user_id/conversations', to: 'memberships#index', as: 'users_memberships'
+  post '/conversations/:conversation_id/users', to: 'memberships#create', as: 'memberships_users'
+  delete '/conversations/:conversation_id/users', to: 'memberships#destroy', as: 'membership_destroy'
 
   resources :direct_messages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
