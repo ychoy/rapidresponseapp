76cd347c03b42dc68445cdedf310977a6b14139c
Yan Yin Choy <yanyinchoy@gmail.com>
2017-04-02 09:18:48 -0700

    added dm routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'users#index'
   # Serve websocket cable requests in-process
   mount ActionCable.server => '/cable'
-  
+
   ## SESSIONS ##
   get "/login", to: "sessions#new"
 
@@ -12,6 +12,10 @@ Rails.application.routes.draw do
 
   resources :users, except: [:index]
 
+  resources :conversations, except: [:edit, :update, :destroy]
 
+  resources :messages, except: [:edit, :update, :destroy]
+
+  resources :direct_messages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
