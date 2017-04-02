36b9b0cee1257bfcae7ae51b6bf52e88e05d4384
Yan Yin Choy <yanyinchoy@gmail.com>
2017-04-02 07:54:29 -0700

    basic set up conversations models

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,3 +1,35 @@
 class Conversation < ApplicationRecord
-  belongs_to :user
+  has_many :messages, dependent: :destroy
+  has_many :users, through: :messages
+
+  def self.direct_message_for_users(users)
+	 	user_firstnames = users.map(&:first_name).sort
+	 	title = "#{user_firstnames.join(", ")}" #TODO - brittle, fix it!
+
+	 	if conversation = conversation.direct_messages.where(title: title).first
+	 		conversation
+	 	else
+	 		# create a new conversation
+	 		conversation = Conversation.new(title: title, direct_message: true)
+	 		conversation.users = users
+	 		conversation.save
+	 		conversation
+	 	end
+	 end
+
+	 def all_members(conversation)
+		 conversation.users = users
+	 end
+
+	 def member?(user)
+		 user.conversations.include?(self)
+	 end
+
+	 def admin?(user)
+	 	return true if self.admin == user.id
+	 end
+
+	 def is_direct_message?
+	 	return self.direct_message
+	 end
 end
